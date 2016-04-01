3620c8e2247a2455eb82c05057a0e7fd9b50a1d8
freddiecabrera <freddiecabrera18@yahoo.com>
2016-03-31 22:41:41 -0700

    make a github profile card

--- app/index.js
+++ app/index.js
@@ -2,15 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from 'react-materialize'
 
+var USER_DATA = {
+  name: 'Freddie Cabrera',
+  username: 'freddiecabrera',
+  image: 'https://avatars1.githubusercontent.com/u/10556877?v=3&s=460'
+}
 
-const HelloWorld = React.createClass({
+const ProfilePic = React.createClass ({
   render () {
     return (
       <div>
-        <Button node='a' waves='light'>Hello World</Button>
+        <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
       </div>
     )
   }
 })
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+const ProfileLink = React.createClass ({
+  render () {
+    return (
+      <div>
+        <a href={`https://www.github.com/${this.props.username}`}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+})
+
+const ProfileName = React.createClass ({
+  render () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+})
+
+const Avatar = React.createClass ({
+  render () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<Avatar user={USER_DATA}/>, document.getElementById('app'))
