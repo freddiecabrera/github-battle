dd936a1f8e7d2065be10b9194ec75c548d558951
freddiecabrera <freddiecabrera18@yahoo.com>
2016-04-04 16:25:19 -0700

    setup ui for results page

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import { centerText, btnMargin, centered } from '../styles'
+import UserDetails from './UserDetails'
+import UserDetailsWrapper from './UserDetailsWrapper'
+import { Link } from 'react-router'
 const PropTypes = React.PropTypes
 
-const puke = obj => {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
-}
-
 const Results = props => {
+  let winningIndex = props.scores[0] > props.scores[1] ? 0 : 1
+  let losingIndex = winningIndex === 0 ? 1 : 0
   return (
-    <div>{puke(props)}</div>
+      <div className='container'>
+        <div calssName='row'>
+          <div className='col s12' style={centerText}>
+            <h4>Results</h4>
+            <div className='row'>
+              <div className='col s12'>
+                <UserDetailsWrapper header='Top Dog'>
+                  <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+                </UserDetailsWrapper>
+                <UserDetailsWrapper header='Loser'>
+                <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+                </UserDetailsWrapper>
+              </div>
+            </div>
+            <div className='col s12'>
+              <Link to='/playerOne'>
+                <button type='button' className="waves-effect waves-light btn pink accent-3" style={btnMargin}>New game</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
